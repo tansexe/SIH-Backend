496617039a5b574bf33cb0ac8ce24339878e08ee
Taniya Nawal Pathak <112875959+tansexe@users.noreply.github.com>
2024-08-24 17:32:38 +0530

    .env changed

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+app.listen(PORT, (error) => {
+  if (!error)
+    console.log(
+      "Server is Successfully Running, and App is listening on port " + PORT
+    );
+  else console.log("Error occurred, server can't start", error);
+});
